8516b0811e613c9bf659553cc1d6a4abd7a7ca06
Arjun Kapoor <52744007+arjunkapoor19@users.noreply.github.com>
2024-10-20 13:10:55 +0530

    Added the layout of the voting page.tsx

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "../../../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
+import Head from "next/head";
+import './style.css';
 
 export default function Home() {
   const router = useRouter();
@@ -37,6 +39,15 @@ export default function Home() {
 
   return (
     <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
+        <title>Breeze 2025 Theme Voting</title>
+      </Head>
+
       {user ? (
         <div>
           <p>Welcome, {user.name} ({user.email})</p>
@@ -45,6 +56,53 @@ export default function Home() {
       ) : (
         <p>Redirecting to login...</p>
       )}
+
+      {/* Section 1*/}
+      <section className="header-section">
+        <div className="header-content">
+          <h1>BREEZE</h1>
+          <h1>
+            <div className="year">2025</div>
+          </h1>
+          <p>Theme Voting</p>
+        </div>
+      </section>
+
+      {/* Section 2*/}
+      <section className="countdown-section">
+        <div className="countdown-content">
+          <div className="circle">
+            <h2>04</h2>
+            <p>Months Left</p>
+          </div>
+          <p>Scroll to Vote</p>
+        </div>
+      </section>
+
+      {/* Section 3*/}
+      <section className="voting-section">
+        <h2>Here are your options</h2>
+        <div className="options-container">
+          
+          <div className="option-card">
+            <div className="box"></div>
+            <h3>Techno-Cyberpunk</h3>
+            <button>Vote</button>
+          </div>
+          
+          <div className="option-card">
+            <div className="box"></div>
+            <h3>90's Sensations</h3>
+            <button>Vote</button>
+          </div>
+          
+          <div className="option-card">
+            <div className="box"></div>
+            <h3>Modern Desi</h3>
+            <button>Vote</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
